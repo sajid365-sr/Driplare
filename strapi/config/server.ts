466bd8d6d19faa775ffab15a2adbce5b0341ea00
sajid365-sr <sajidsorker86@gmail.com>
2024-09-27 02:54:12 +0600

    13

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -1,7 +1,13 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+interface Env {
+  (key: string, defaultValue?: string | number | boolean): string | undefined;
+  int(key: string, defaultValue?: number): number;
+  array(key: string): string[];
+}
+
+export default ({ env }: { env: Env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
