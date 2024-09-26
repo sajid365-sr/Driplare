343b909bf0b0b6f2929cf01a41a732770539457f
sajid365-sr <sajidsorker86@gmail.com>
2024-09-26 09:10:32 +0600

    Strapi CMS added

--- app/api/subscribe/route.js
+++ app/api/subscribe/route.js
@@ -3,18 +3,9 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
-export async function POST(res) {
-  try {
-    await resend.emails.send({
-      from: "Your Company <onboarding@resend.dev>",
-      to: res.body.email,
-      subject: "Thanks for subscribing!",
-      html: "<p>We'll keep you updated on our progress.</p>",
-    });
+export async function POST(req, {}) {
+  const { email } = await req.json();
 
-    NextResponse.json({ message: "Subscribed successfully" });
-  } catch (error) {
-    console.error("Subscription error:", error);
-    NextResponse.json({ error: "Failed to subscribe" });
-  }
+  // console.log(email);
+  return NextResponse.json("Test Successful");
 }
