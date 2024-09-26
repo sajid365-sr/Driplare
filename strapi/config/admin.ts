7100dfed9ecab3a59dee0f4b129b89098f63fcca
sajid365-sr <sajidsorker86@gmail.com>
2024-09-26 09:21:55 +0600

    admin.ts file edited

--- strapi/config/admin.ts
+++ strapi/config/admin.ts
@@ -1,17 +1,23 @@
-export default ({ env }) => ({
+type EnvFunction = {
+  (key: string): string;
+  bool(key: string, defaultValue: boolean): boolean;
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({ env }: { env: EnvFunction }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT"),
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
 });
